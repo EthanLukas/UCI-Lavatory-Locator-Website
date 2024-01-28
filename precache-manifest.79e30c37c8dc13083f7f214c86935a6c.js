b350b9a4d6b45a52201c37509f48a65748d122e6
Ethan Lukas <elukas@uci.edu>
2024-01-28 01:51:54 -0800

    Updates

---
+++ precache-manifest.79e30c37c8dc13083f7f214c86935a6c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11104020b304fee83fa1001ca94ef79e",
+    "url": "/UCI-Lavatory-Locator/index.html"
+  },
+  {
+    "revision": "26887c190b81533b15d3",
+    "url": "/UCI-Lavatory-Locator/static/css/main.954acfa4.chunk.css"
+  },
+  {
+    "revision": "944e068eb4da1104dd59",
+    "url": "/UCI-Lavatory-Locator/static/js/2.895b1b92.chunk.js"
+  },
+  {
+    "revision": "8bf7614a6420c3456690",
+    "url": "/UCI-Lavatory-Locator/static/js/3.2d98378e.chunk.js"
+  },
+  {
+    "revision": "26887c190b81533b15d3",
+    "url": "/UCI-Lavatory-Locator/static/js/main.f7c29c08.chunk.js"
+  },
+  {
+    "revision": "622f8e28be7becdb1052",
+    "url": "/UCI-Lavatory-Locator/static/js/runtime~main.56e7c70e.js"
+  },
+  {
+    "revision": "fd23f3dbf0d04f0ea31709e9e2324117",
+    "url": "/UCI-Lavatory-Locator/static/media/bathroom.fd23f3db.png"
+  },
+  {
+    "revision": "554aaca77228f5a4016fac961b99a235",
+    "url": "/UCI-Lavatory-Locator/static/media/bathroomSS.554aaca7.jpeg"
+  },
+  {
+    "revision": "9d1ba1714b323b78634609628eb2bb2a",
+    "url": "/UCI-Lavatory-Locator/static/media/campus.9d1ba171.jpg"
+  },
+  {
+    "revision": "996bb85352f0c54de2c58e8151f82db1",
+    "url": "/UCI-Lavatory-Locator/static/media/ethan.996bb853.jpg"
+  },
+  {
+    "revision": "9634514c83f823bde0d5506a72c8af5c",
+    "url": "/UCI-Lavatory-Locator/static/media/lanni.9634514c.JPG"
+  },
+  {
+    "revision": "1df3a3449ae046b9754318d373d5bc84",
+    "url": "/UCI-Lavatory-Locator/static/media/linkedin.1df3a344.png"
+  },
+  {
+    "revision": "0fa73e68d0a87266faa5314205726fd3",
+    "url": "/UCI-Lavatory-Locator/static/media/mapScreenshot.0fa73e68.jpeg"
+  },
+  {
+    "revision": "3652f45c0f854f7180e010b2cec0357f",
+    "url": "/UCI-Lavatory-Locator/static/media/philip.3652f45c.jpg"
+  },
+  {
+    "revision": "d78f65c494983bf5007e07e81f163012",
+    "url": "/UCI-Lavatory-Locator/static/media/simar.d78f65c4.PNG"
+  },
+  {
+    "revision": "aa1db1520ab06cf02a48ab6eda069389",
+    "url": "/UCI-Lavatory-Locator/static/media/uciLogo.aa1db152.png"
+  }
+]);
